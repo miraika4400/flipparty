9fabd0f2c07ec97d046b21f5b0605a69f5288f8b
KUDOAYANO <ayanokudo1211@gmail.com>
2021-07-02 14:26:29 +0900

    【更新】　記憶ゲームの更新

--- project/flipparty/flipparty/player_remember.cpp
+++ project/flipparty/flipparty/player_remember.cpp
@@ -13,3 +13,25 @@ CPlayerRemember::CPlayerRemember()
 CPlayerRemember::~CPlayerRemember()
 {
 }
+
+CPlayerRemember * CPlayerRemember::Create(void)
+{
+    return nullptr;
+}
+
+HRESULT CPlayerRemember::Init(void)
+{
+    return E_NOTIMPL;
+}
+
+void CPlayerRemember::Uninit(void)
+{
+}
+
+void CPlayerRemember::Update(void)
+{
+}
+
+void CPlayerRemember::Draw(void)
+{
+}
